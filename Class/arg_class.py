072284da0abe5040c46b7a52a5d1244921557e75
moyac1012 <yuto.tamori@meetasu.jp>
2022-04-13 17:58:13 +0900

    save

--- Class/arg_class.py
+++ Class/arg_class.py
@@ -8,7 +8,7 @@ class arg_class:
         parser.add_argument('INPUT_IMAGE', help="Please set the image directory.")
         parser.add_argument('-s', '--size', help='Option when you want to set image size.', type=int, default=640)
         parser.add_argument('-d', '--DataArgment', 
-            help='Option when you want to "Data Argmentation". The types are now salt noise, pepper noise, smoothing noise. For example, if you want to use salt noise and smooth noise, use "-d salt smooth". (It\'s okay to shorten it like "-d sa sm")', nargs='*')
+            help='Option when you want to "Data Argmentation". The types are now salt noise, pepper noise, smoothing noise, fliplr. For example, if you want to use salt noise and smooth noise, use "-d salt smooth". (It\'s okay to shorten it like "-d sa sm")', nargs='*')
         args = parser.parse_args()
 
         return(args)
